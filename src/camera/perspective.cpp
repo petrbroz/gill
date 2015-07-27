69268638bc2861aa0825579588306cadf93d39fc
Petr Broz <petr.broz@autodesk.com>
2015-07-27 10:14:41 -0700

    Added depth of field.

--- src/camera/perspective.cpp
+++ src/camera/perspective.cpp
@@ -14,14 +14,12 @@ Ray PerspectiveCamera::generate_ray(const Camera::Sample &sample) const {
     Point cam_p = (*_film_to_cam)(img_p);
     Ray ray(Point(0.0), normalize(Vector(cam_p.x, cam_p.y, cam_p.z)));
 
-    /*
     if (_lens_radius > 0.0) {
         float focus_t = _focal_dist / ray.d.z;
         Point focus_p = ray(focus_t);
-        ray.o = Point(sample.lens_u, sample.lens_v, 0.0); // TODO: map lens_u and lens_v properly
+        ray.o = Point(sample.lens_u * _lens_radius, sample.lens_v * _lens_radius, 0.0); // TODO: map lens_u and lens_v properly
         ray.d = normalize(focus_p - ray.o);
     }
-    */
     return (*_ltow)(ray);
 }
 
--- src/core/scene.cpp
+++ src/core/scene.cpp
@@ -24,21 +24,29 @@ void Scene::capture() {
     int res_x = _camera->_film->_xres;
     int res_y = _camera->_film->_yres;
 
+    const int samples = 64;
+
     cout << "P3" << endl;
     cout << res_x << " " << res_y << endl;
     cout << "255" << endl;
     for (int y = 0; y < res_y; ++y) {
         for (int x = 0; x < res_x; ++x) {
-            Camera::Sample sample = {(float)x, (float)(res_y - y), 0.0, 0.0};
-            Ray ray = _camera->generate_ray(sample);
-            if (intersect(ray, t, &si)) {
-                Normal n = normalize(si.pi.gi.n);
-                cout << (int)(n.x * 127 + 128) << " ";
-                cout << (int)(n.y * 127 + 128) << " ";
-                cout << (int)(n.z * 127 + 128) << " ";
-            } else {
-                cout << "0 0 0 ";
+            int r = 0, g = 0, b = 0;
+            for (int s = 0; s < samples; ++s) {
+                float u = 2.0 * rand() / RAND_MAX - 1.0;
+                float v = 2.0 * rand() / RAND_MAX - 1.0;
+                Camera::Sample sample = {(float)x, (float)(res_y - y), u, v};
+                Ray ray = _camera->generate_ray(sample);
+                if (intersect(ray, t, &si)) {
+                    Normal n = normalize(si.pi.gi.n);
+                    r += (int)(n.x * 127 + 128);
+                    g += (int)(n.y * 127 + 128);
+                    b += (int)(n.z * 127 + 128);
+                }
             }
+            cout << (r / samples) << " ";
+            cout << (g / samples) << " ";
+            cout << (b / samples) << " ";
         }
         cout << endl;
     }
