e34fbf44ab0c36dcfcb02fe78ffe2c85d16a4e1e
Petr Broz <petr.broz@autodesk.com>
2015-07-28 08:14:29 -0700

    Don't run a thread for a single tile.

--- src/renderer/sampler.cpp
+++ src/renderer/sampler.cpp
@@ -45,18 +45,22 @@ void Sampler::render(const Aggregate *aggregate, const Camera *camera) const {
     int seg_y = res_y / _thread_tiles[1];
 
     auto begin_time = clock();
-    vector<thread> threads;
-    for (int j = 0; j < _thread_tiles[1]; ++j) {
-        int min_y = j * seg_y;
-        int max_y = (j + 1) * seg_y - 1;
-        for (int i = 0; i < _thread_tiles[0]; ++i) {
-            int min_x = i * seg_x;
-            int max_x = (i + 1) * seg_x - 1;
-            threads.push_back(thread(render_fragment, aggregate, camera, min_x, min_y, max_x, max_y));
+    if (_thread_tiles[0] > 1 || _thread_tiles[1] > 1) {
+        vector<thread> threads;
+        for (int j = 0; j < _thread_tiles[1]; ++j) {
+            int min_y = j * seg_y;
+            int max_y = (j + 1) * seg_y - 1;
+            for (int i = 0; i < _thread_tiles[0]; ++i) {
+                int min_x = i * seg_x;
+                int max_x = (i + 1) * seg_x - 1;
+                threads.push_back(thread(render_fragment, aggregate, camera, min_x, min_y, max_x, max_y));
+            }
         }
-    }
-    for (thread &t : threads) {
-        t.join();
+        for (thread &t : threads) {
+            t.join();
+        }
+    } else {
+        render_fragment(aggregate, camera, 0, 0, res_x - 1, res_y - 1);
     }
     auto end_time = clock();
 
