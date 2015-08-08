6aafe2087e097ff7897072b598e03625857ff041
Petr Broz <petr.broz@autodesk.com>
2015-08-07 22:49:50 -0700

    Cleaned up the parser code.

--- src/core/parser.h
+++ src/core/parser.h
@@ -4,6 +4,7 @@
 #include <yaml.h>
 #include <vector>
 #include <map>
+#include <functional>
 
 #include "core/aggregate.h"
 #include "core/camera.h"
@@ -27,21 +28,27 @@ public:
     std::shared_ptr<Scene> next_scene();
 
 protected:
-    yaml_parser_t _parser;
     FILE *_input;
-    std::map<int, std::shared_ptr<Geometry>> _geometry_cache;
-    std::map<int, std::shared_ptr<Material>> _material_cache;
-    std::map<int, std::shared_ptr<Transform>> _transform_cache;
+    yaml_parser_t _parser;
+    yaml_document_t _document;
+    std::map<int, std::shared_ptr<Transform>> _transforms;
+    std::map<int, std::shared_ptr<Geometry>> _geometries;
+    std::map<int, std::shared_ptr<Material>> _materials;
 
-    std::shared_ptr<Scene> parse_scene(yaml_document_t *doc, yaml_node_t *node);
-    std::shared_ptr<Aggregate> parse_primitives(yaml_document_t *doc, yaml_node_t *node);
-    Primitive parse_primitive(yaml_document_t *doc, yaml_node_t *node);
-    std::shared_ptr<Geometry> parse_geometry(yaml_document_t *doc, yaml_node_t *node);
-    std::shared_ptr<Material> parse_material(yaml_document_t *doc, yaml_node_t *node);
-    std::shared_ptr<Transform> parse_transform(yaml_document_t *doc, yaml_node_t *node);
-    std::shared_ptr<Camera> parse_camera(yaml_document_t *doc, yaml_node_t *node);
-    std::shared_ptr<Film> parse_film(yaml_document_t *doc, yaml_node_t *node);
-    std::shared_ptr<Renderer> parse_renderer(yaml_document_t *doc, yaml_node_t *node);
+    std::shared_ptr<Scene> parse_scene(yaml_node_t *node);
+    std::shared_ptr<Aggregate> parse_primitives(yaml_node_t *node);
+    Primitive parse_primitive(yaml_node_t *node);
+    std::shared_ptr<Geometry> parse_geometry(yaml_node_t *node);
+    std::shared_ptr<Material> parse_material(yaml_node_t *node);
+    std::shared_ptr<Transform> parse_transform(yaml_node_t *node);
+    std::shared_ptr<Camera> parse_camera(yaml_node_t *node);
+    std::shared_ptr<Film> parse_film(yaml_node_t *node);
+    std::shared_ptr<Renderer> parse_renderer(yaml_node_t *node);
+
+    void _traverse_mapping(yaml_node_t *node, std::function<void(std::string&, yaml_node_t*)> func);
+    void _traverse_sequence(yaml_node_t *node, std::function<void(yaml_node_t*)> func);
+    template <typename T> T _get_scalar(yaml_node_t *node);
+    template <typename T, int size> std::vector<T> _get_sequence(yaml_node_t *node);
 };
 
 }}
